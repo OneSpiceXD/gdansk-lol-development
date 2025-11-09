3b89e0906b50ef61841d1e36bb66db630011f7ed
OneSpiceXD <pedrohas.93@hotmail.com>
2025-11-09 17:27:59 +0100

    Fix: Use v2 CSS styling for main route

--- gdansk-league/app/layout.tsx
+++ gdansk-league/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./v2/globals-v2.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import ClientLayout from "@/components/ClientLayout";
 
